a8781e49628be53e7cbfe917f10bac29f55476e7
JorgeDoicela <jorge.doicela@istpet.edu.ec>
2026-01-05 22:07:21 -0500

    Usuario y Producto

--- order_service/src/repository/order.repository.js
+++ order_service/src/repository/order.repository.js
@@ -4,13 +4,13 @@ exports.obtenerTodos = async () => (await db.query("SELECT * FROM ordenes")).row
 exports.obtenerPorId = async (id) => (await db.query("SELECT * FROM ordenes WHERE id = $1", [id])).rows[0];
 
 exports.crear = async (orden) => (await db.query(
-    "INSERT INTO ordenes (user_id, product_id, cantidad) VALUES ($1, $2, $3) RETURNING *",
-    [orden.user_id, orden.product_id, orden.cantidad]
+    "INSERT INTO ordenes (usuario_id, producto_id, cantidad) VALUES ($1, $2, $3) RETURNING *",
+    [orden.usuario_id, orden.producto_id, orden.cantidad]
 )).rows[0];
 
 exports.actualizar = async (id, orden) => (await db.query(
-    "UPDATE ordenes SET user_id = $1, product_id = $2, cantidad = $3 WHERE id = $4 RETURNING *",
-    [orden.user_id, orden.product_id, orden.cantidad, id]
+    "UPDATE ordenes SET usuario_id = $1, producto_id = $2, cantidad = $3 WHERE id = $4 RETURNING *",
+    [orden.usuario_id, orden.producto_id, orden.cantidad, id]
 )).rows[0];
 
 exports.eliminar = async (id) => (await db.query("DELETE FROM ordenes WHERE id = $1 RETURNING *", [id])).rows[0];
--- order_service/src/service/order.service.js
+++ order_service/src/service/order.service.js
@@ -4,7 +4,7 @@ exports.obtenerTodos = repositorio.obtenerTodos;
 exports.obtenerPorId = repositorio.obtenerPorId;
 
 exports.crear = async (orden) => {
-    if (!orden.user_id || !orden.product_id || !orden.cantidad) throw new Error("Datos incompletos");
+    if (!orden.usuario_id || !orden.producto_id || !orden.cantidad) throw new Error("Datos incompletos");
     if (orden.cantidad <= 0) throw new Error("Cantidad inválida");
     if (orden.cantidad > 10) throw new Error("Regla de Negocio: Máximo 10 unidades por producto");
     return await repositorio.crear(orden);
